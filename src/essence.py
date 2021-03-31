0ffdfe8eb4e32bdeda9a6a6779351b09ccc5f564
youngsuk-cys <k980899@gmail.com>
2021-03-31 16:42:51 +0900

    git commit

--- src/essence.py
+++ src/essence.py
@@ -1,6 +1,20 @@
-a= 1+1
-b= 1+2
-print(a)
-print(b)
+# 더하기 
+print (1+1)
 
-1+2
+# 빼기
+print (1-1)
+
+# 곱하기
+print (1*2)
+
+# 나누기
+print (1/2)
+
+# 나누기 소숫점 버림 python 3 에서 인식 X
+# print (1//2)
+
+# 나머지
+print ( 1 % 3)
+
+
+divmod ( 50 , 8)
---
+++ src/variable.py
@@ -0,0 +1,16 @@
+watch = 10000
+lighter = 100 
+
+watch + lighter
+
+price = 5000 
+price = price * 0.5
+price *= 0.5
+price
+
+name = 'pig'
+name
+b = "dad"
+b
+
+name + '_' + b
